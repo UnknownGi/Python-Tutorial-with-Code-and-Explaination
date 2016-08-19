3fe24bdd08de95f47125b5e7489368d118476226
Danyal Imran <danz1ka@users.noreply.github.com>
2016-08-20 00:43:07 +0500

    Comments.py and Numbers.py Added
    Added 3.0.1 and 3.1.1 Chapters

---
+++ Chapter #3 - Informal Introduction to Python/3.0.1 - Comments.py
@@ -0,0 +1,10 @@
+# Comments in Python
+
+# In Python, Comments are added with the hash(#) symbol.
+# Comments are added for code visibility and function description, input parameters, output parameters
+
+'''
+Multi comments in Python can be created using three quotations
+The purpose of multi-comments is to describe about some functionality in more than one line
+These are more visible in code.
+'''
